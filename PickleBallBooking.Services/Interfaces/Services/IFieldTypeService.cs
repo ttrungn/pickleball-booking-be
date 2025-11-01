5360972fe7a4e30ed37fdb3123be1b23becfbc2e
khanhhoang209 <lehoangkhanh209@gmail.com>
2025-11-01 17:23:11 +0700

    feat: add get field types api

---
+++ PickleBallBooking.Services/Interfaces/Services/IFieldTypeService.cs
@@ -0,0 +1,11 @@
+﻿using PickleBallBooking.Services.Features.FieldTypes.Queries.GetFieldTypes;
+using PickleBallBooking.Services.Models.Responses;
+
+namespace PickleBallBooking.Services.Interfaces.Services;
+
+public interface IFieldTypeService
+{
+    Task<PaginatedServiceResponse<FieldTypeResponse>> GetFieldTypesAsync(
+        GetFieldTypesQuery query,
+        CancellationToken cancellationToken = default);
+}
---
+++ PickleBallBooking.Services/Models/Requests/FieldTypeGetRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PickleBallBooking.Services.Models.Requests;
+
+public class FieldTypeGetRequest
+{
+    public string? Name { get; init; } = string.Empty;
+    public bool? IsActive { get; init; } = true;
+    public int? PageNumber { get; init; } = 1;
+    public int? PageSize { get; init; } = 8;
+}
